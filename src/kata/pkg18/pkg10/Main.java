8c0352c231032c2bc600b438fdda216a2c9ac531
usuario <usuario@10.22.146.196>
2013-10-18 11:10:36 +0100

    MailReader

--- src/kata/pkg18/pkg10/Main.java
+++ src/kata/pkg18/pkg10/Main.java
@@ -3,9 +3,10 @@ package kata.pkg18.pkg10;
 public class Main {
 
     public static void main(String[] args) {
-        HistogramBuilder<Integer> builder = new HistogramBuilder();
-        builder.calculate(new Integer[] {1,2,3,5,1,541,1,5,1,5,1,3,8,3,9,56,46,25,13,81,45,77,88,63});
-        HistogramViewer viewer = new HistogramViewer(builder.getHistogram());
+        String filename = args[0];
+        HistogramBuilder<String> builder = new HistogramBuilder();
+        builder.calculate(MailReader.readDomains(filename));
+        HistogramViewer<String> viewer = new HistogramViewer(builder.getHistogram());
         viewer.show();
     }
 }
