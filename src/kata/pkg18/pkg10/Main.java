f015c176e823f5c9e80fac6bc606954958db65cb
usuario <usuario@10.22.146.196>
2013-10-18 11:22:52 +0100

    Con la clase de control

--- src/kata/pkg18/pkg10/Main.java
+++ src/kata/pkg18/pkg10/Main.java
@@ -3,10 +3,7 @@ package kata.pkg18.pkg10;
 public class Main {
 
     public static void main(String[] args) {
-        String filename = args[0];
-        HistogramBuilder<String> builder = new HistogramBuilder();
-        builder.calculate(MailReader.readDomains(filename));
-        HistogramViewer<String> viewer = new HistogramViewer(builder.getHistogram());
-        viewer.show();
+        CalculateMailDomainHistogramControl control = new CalculateMailDomainHistogramControl(args[0]);
+        control.execute();
     }
 }
